83f506382f4a1f600fa6b8a0fb01d617a950e1bb
esxpots <esxpots@gmail.com>
2026-02-06 07:08:22 +0300

    Vercel config fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // TypeScript hatalarını görmezden gel (Build sırasında)
   typescript: {
     ignoreBuildErrors: true,
   },
-  // ESLint hatalarını görmezden gel (Build sırasında)
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Hostinger veya dış kaynaklı resimler için
   images: {
     unoptimized: true, 
     remotePatterns: [
